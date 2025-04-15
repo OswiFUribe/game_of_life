cf241820d1abb767dfe63971fc5037bb263ee6d5
Oswaldo Fonseca <oe.fonsecauribe@ugto.,mx>
2025-04-15 15:52:31 -0600

    feat: agrega un archivo para administrar los módulos del proyecto, incluido el nuevo grid creado.

---
+++ frontend/src/app/app.modules.ts
@@ -0,0 +1,19 @@
+// app.module.ts
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { GridComponent } from './grid/grid.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GridComponent 
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
