498139190cf5a3ecd42f841aa63bf48256eb0bdc
Oswaldo Fonseca <oe.fonsecauribe@ugto.,mx>
2025-04-15 16:15:44 -0600

    feat(grid): creación inicial del componente grid para recibir de la API del back.

--- frontend/src/app/grid/grid.component.ts
+++ frontend/src/app/grid/grid.component.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SimulationService, GridResponse } from '../simulation.service';
 
 @Component({
   selector: 'app-grid',
-  imports: [],
   templateUrl: './grid.component.html',
-  styleUrl: './grid.component.css'
+  styleUrls: ['./grid.component.css']
 })
-export class GridComponent {
+export class GridComponent implements OnInit {
+  grid: number[][] = [];
 
+  constructor(private simulationService: SimulationService) {}
+
+  ngOnInit(): void {
+    this.loadGrid();
+  }
+
+  loadGrid(): void {
+    this.simulationService.getGrid().subscribe((data: GridResponse) => {
+      this.grid = data.grid;
+    });
+  }
+
+  onReset(pattern: string = 'glider'): void {
+    this.simulationService.resetSimulation(pattern).subscribe((data: GridResponse) => {
+      this.grid = data.grid;
+    });
+  }
+
+  onStep(): void {
+    this.simulationService.stepSimulation().subscribe((data: GridResponse) => {
+      this.grid = data.grid;
+    });
+  }
+
+  onBack(): void {
+    this.simulationService.backSimulation().subscribe((data: GridResponse) => {
+      this.grid = data.grid;
+    });
+  }
+
+  onStart(): void {
+    this.simulationService.startSimulation().subscribe((data: GridResponse) => {
+      this.grid = data.grid;
+    });
+  }
+
+  onPause(): void {
+    this.simulationService.pauseSimulation().subscribe((data: GridResponse) => {
+      this.grid = data.grid;
+    });
+  }
 }
